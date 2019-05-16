b56ef8945fe6a49eb777a6b78934cb3a8c4f6fce
MinenhleS <ngwenyasiyamukela@gmail.com>
2019-05-16 12:32:25 +0200

    updating person and learner class

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,4 +1,17 @@
 public class Teacher extends Person{
 
+    int numberOfSubjects;
+
+    //constructor
+    public Teacher(String firstName, String lastName, String email, int numberOfSubjects) {
+        super(firstName, lastName,email);
+        this.numberOfSubjects = numberOfSubjects;
+
+    }
+
+    //Accessor method for getting number of subject learner enrolled in & tokens
+    public int getNumberOfSubjects(){
+        return numberOfSubjects;
+    }
 
 }
