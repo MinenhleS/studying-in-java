19efc71a064254883dfd144400715e6cc038c480
MinenhleS <ngwenyasiyamukela@gmail.com>
2019-05-20 17:02:01 +0200

    updating person and learner class

--- src/main/java/net/studies/Lesson.java
+++ src/main/java/net/studies/Lesson.java
@@ -1,5 +1,13 @@
 package net.studies;
 
+import java.util.ArrayList;
+
 public class Lesson {
+
+    ArrayList attendingList = new ArrayList();
+
+    public void accept(Learner learner){
+
+    }
 
 }
